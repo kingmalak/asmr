278893c2dae9ffc5f2377736a5e1adfe2f860d81
unknown <ahmaddkhaleed510@gmail.com>
2021-11-28 17:07:53 +0200

    Second Commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,19 @@ function App() {
   const [SeatsAvailableOnFlight, setSeatsAvailableOnFlight] = useState("");
   const [listOfFlights, setListOfFlights] = useState([]);
 
+    //insert
+    const addFlight = () => {
+      Axios.post("http://localhost:3007/addflight", {From: From, To: To,
+      FlightDate: FlightDate, Cabin: Cabin, SeatsAvailableOnFlight: SeatsAvailableOnFlight})
+      .then(() => {
+        setListOfFlights([...listOfFlights, {From: From, To: To,
+          FlightDate: FlightDate, Cabin: Cabin, SeatsAvailableOnFlight: SeatsAvailableOnFlight}])
+      })
+      .catch(() => {
+        console.log("ERR");
+      });
+    };
+    
   //display
   useEffect(() => {
     Axios.get("http://localhost:3007/read")
@@ -26,6 +39,25 @@ function App() {
   
   return (
   <div className= "App">
+      <div className = "inputs">
+      <p>From:</p><br></br><input type= "text" placeholder="Departs from" 
+      onChange = {(event) => {setFrom(event.target.value);}}/><br></br>
+      
+      <p>To:</p><br></br><input type= "text" placeholder="Arrives in" 
+      onChange = {(event) => {setTo(event.target.value);}}/><br></br>
+      
+      <p>Flight Date:</p><br></br><input type= "text" placeholder="Flight date" 
+      onChange = {(event) => {setFlightDate(event.target.value);}}/><br></br>
+      
+      <p>Cabin:</p><br></br><input type= "text" placeholder="Cabin" 
+      onChange = {(event) => {setCabin(event.target.value);}}/><br></br>
+      
+      <p>Seats Available on Flight:</p><br></br><input type= "text" placeholder="Seats Available on Flight" 
+      onChange = {(event) => {setSeatsAvailableOnFlight(event.target.value);}}/><br></br><br></br>
+      
+      <button onClick = {addFlight}>Add Flight</button>
+    </div>
+
     <div className = "listOfFlights">
     {listOfFlights.map((val) => {
       return (
--- server/index.js
+++ server/index.js
@@ -10,7 +10,26 @@ app.use(cors());
 
 mongoose.connect('mongodb+srv://userito:passwordito@cluster0.j4smg.mongodb.net/airlines?authSource=admin&replicaSet=atlas-s0bcyn-shard-0&readPreference=primary&ssl=true', 
     {useNewUrlParser: true}).then(result => console.log("mongodb is connected")).catch(error => console.log(error)) 
+
+//insert    
+app.post("/addflight", async (req,res) => {
+    const From = req.body.From;
+    const To = req.body.To;
+    const FlightDate = req.body.FlightDate;
+    const Cabin = req.body.Cabin;
+    const SeatsAvailableOnFlight = req.body.SeatsAvailableOnFlight;
     
+    const newFlight = new FlightModel({
+        From: From,
+        To: To,
+        FlightDate: FlightDate,
+        Cabin: Cabin,
+        SeatsAvailableOnFlight: SeatsAvailableOnFlight
+        });
+    await newFlight.save();
+    res.send("Success!");
+    });    
+            
 //display
 app.get("/read" , async (req,res) => {
     FlightModel.find({}, (err,result) => {
